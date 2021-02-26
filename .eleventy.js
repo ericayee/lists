a10909d807536b86e64cbf07a7be8f0cd522ac99
Erica Yee <23279506+ericayee@users.noreply.github.com>
2021-02-26 15:16:32 -0800

    remove favicon

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight"),
       markdownIt = require('markdown-it');
 module.exports = eleventyConfig => {
   eleventyConfig.addPlugin(syntaxHighlight);
-  eleventyConfig.addPassthroughCopy("{{ 'favicon.ico' | url }}");
   const options = {
     html: true,
     breaks: true,
