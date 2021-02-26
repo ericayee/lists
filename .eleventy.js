9b064a2b8ec7b311ec29b4f8fb476cf28caa01d9
Erica Yee <23279506+ericayee@users.noreply.github.com>
2021-02-26 14:54:13 -0800

    trying favicon again + fix 404 back link

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight"),
       markdownIt = require('markdown-it');
 module.exports = eleventyConfig => {
   eleventyConfig.addPlugin(syntaxHighlight);
-  // eleventyConfig.addPassthroughCopy('favicon.ico');
+  eleventyConfig.addPassthroughCopy('favicon.ico');
   const options = {
     html: true,
     breaks: true,
